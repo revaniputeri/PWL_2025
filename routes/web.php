2287380a10262036a504ff9daf19fdaeb6c7bc11
Revani Nanda Putri <revaninandaputeri@gmail.com>
2025-04-29 10:32:08 +0700

    feat: implement ajax for editing users

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}', [UserController::class, 'show']);
     Route::get('/{id}/edit', [UserController::class, 'edit']);
     Route::put('/{id}', [UserController::class, 'update']);
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); //Menampilkan halaman form edit user Ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); //Menyimpan halaman form edit user ajax
     Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
