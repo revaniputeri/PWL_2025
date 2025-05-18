29d8b14fba0b3d5b089b01c2e1824344e0a2f760
revaniputeri <revaninandaputeri@gmail.com>
2025-05-18 12:06:13 +0700

    feat: implement export data barang from excel

--- routes/web.php
+++ routes/web.php
@@ -110,8 +110,9 @@ Route::middleware(['authorize:ADM,MNG'])->group(function () {
         Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete barang Ajax
         Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // Untuk hapus data barang Ajax
         Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data barang
-        Route::get('/import', [BarangController::class, 'import']);
-        Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
+        Route::get('/import', [BarangController::class, 'import']); // ajax form upload excel
+        Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // AJAX import excel
+        Route::get('/export_excel', [BarangController::class, 'export_excel']); // ajax form download excel
     });
 });
 
